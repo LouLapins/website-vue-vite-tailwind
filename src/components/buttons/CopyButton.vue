ce224b5904f27f32d808fe0c53a2a808884c82a6
LouLapins <loulapins@hotmail.com>
2022-05-28 20:30:31 +0200

    Add copy-to-clipboard button

---
+++ src/components/buttons/CopyButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button :class="[copied ? 'bg-green-600' : 'bg-gray-500', 'w-10 h-10 transition-colors duration-300 ease-in-out cursor-pointer rounded-md flex items-center justify-center']">
+            <span class="sr-only">Copy content</span>
+            <i aria-hidden="true"
+                class="text-white transition duration-300 ease-in-out transform">
+                <CopyIcon v-if="!copied"></CopyIcon>
+                <CheckIcon v-else-if="copied"></CheckIcon>
+            </i>
+        </button>
+    </div>
+</template>
+
+<script setup>
+import CopyIcon from '../svgs/CopyIcon.vue'
+import CheckIcon from '../svgs/CheckIcon.vue'
+
+defineProps({
+    copied: Boolean
+})
+
+</script>
--- src/pages/contact/Contact.vue
+++ src/pages/contact/Contact.vue
@@ -3,11 +3,16 @@
         <div class="flex flex-col items-center justify-center w-1/2">
             <h1 class="text-3xl font-bold text-zinc-900 font-display">Let's talk.</h1>
             <a class="my-16" href="mailto:lapins.lou@gmail.com">
-                <ActionButton title="Link to email" text="Email me here" icon="contact" />
+                <ActionButton title="Open email" text="Email me here" icon="contact" />
             </a>
-            <input value="lapins.lou@gmail.com" type="text" readonly />
-            <button @click="copy">Copy</button>
-            <p class="mt-4 mb-16 text-sm font-light text-gray-700 font-body">Or feel free to message me on any of these
+            <p class="mb-4 text-sm font-light text-gray-700 font-body">Or copy my email address here.</p>
+            <div class="flex items-center p-1 bg-gray-100 rounded-lg ring-1 ring-gray-200">
+                <input ref="myInput" v-on:focus="$event.target.select()" value="lapins.lou@gmail.com" type="text"
+                    readonly
+                    class="p-3 mx-2 text-xs text-gray-500 bg-transparent focus-visible:[outline:none] font-body">
+                <CopyButton :copied="copied" @click="copy"></CopyButton>
+            </div>
+            <p class="my-16 text-sm font-light text-gray-700 font-body">Or connect with me on any of these
                 platforms&colon;</p>
             <SocialLinkButtonGroup />
         </div>
@@ -15,11 +20,31 @@
     </section>
 </template>
 <script setup>
+import { ref } from 'vue'
 import ActionButton from '../../components/buttons/ActionButton.vue'
 import SocialLinkButtonGroup from '../../components/buttons/SocialLinkButtonGroup.vue'
+import CopyButton from '../../components/buttons/CopyButton.vue'
 
-function copy() {
-    Clipboard.writeText('hello')
-    console.log(Clipboard)
+const myInput = ref()
+const copied = ref(false)
+
+function triggerCheckMark() {
+    copied.value = true
+    const removeCheckMark = () => {
+        copied.value = false
+        myInput.value.blur()
+    }
+    setTimeout(removeCheckMark, 3000)
 }
+
+async function copy() {
+    try {
+        myInput.value.focus()
+        await navigator.clipboard.writeText(myInput.value.value)
+        triggerCheckMark()
+    } catch ($e) {
+        alert('Sorry, could not copy!')
+    }
+}
+
 </script>
