2148041e5c92a265f0f2b9af866f3442fd6108cf
LouLapins <loulapins@hotmail.com>
2022-05-31 14:47:32 +0200

    Fix wrong image path

--- src/pages/home/components/ImageCircle.vue
+++ src/pages/home/components/ImageCircle.vue
@@ -1,6 +1,6 @@
 <template>
     <Transition enter-from-class="translate-y-24 opacity-0" tag="div" appear>
-      <img ref="image" src="../../assets/lou-river.jpg" alt="Lou Lapins"
+      <img ref="image" src="../../../assets/lou-river.jpg" alt="Lou Lapins"
         class="absolute self-center w-1/3 rounded-full left-1/3">
     </Transition>
 </template>
