7f88ba1ae738bbb22b6f0fddb187cc03958c00c9
LouLapins <loulapins@hotmail.com>
2022-06-12 21:27:46 +0200

    Change landing page background svg

---
+++ src/components/svgs/illustrations/WavyBackground.vue
@@ -0,0 +1,9 @@
+<template>
+    <svg viewBox="0 0 504 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M159 -1.52588e-05C288.5 -198 504.001 -1.52588e-05 504.001 -1.52588e-05L504 1024H0C0 1024 255.138 719.201 256 518C256.883 312.194 29.5 198 159 -1.52588e-05Z"
+            fill="#F5F3F0" />
+    </svg>
+</template>
+<script setup>
+</script>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,8 +2,9 @@
     <div class="flex flex-col items-center w-full mb-8 lg:mb-0 lg:h-[calc(100vh-50px)] justify-evenly lg:flex-row">
         <ImageCircle />
         <TextElements />
-        <div class="relative invisible w-full h-0 overflow-hidden lg:min-h-screen lg:visible">
-            <div class="absolute w-3/5 h-[400%] top-0 bg-right-bottom bg-no-repeat right-0 bg-waves-img">
+        <div class="relative invisible w-full h-0 overflow-hidden lg:h-full lg:visible">
+            <div class="absolute top-0 right-0 w-1/2 h-full">
+                <WavyBackground />
             </div>
         </div>
     </div>
@@ -12,5 +13,8 @@
 <script setup lang="ts">
 import ImageCircle from './components/ImageCircle.vue'
 import TextElements from './components/TextElements.vue'
+import WavyBackground from '../../components/svgs/illustrations/WavyBackground.vue';
 
 </script>
+<style>
+</style>
