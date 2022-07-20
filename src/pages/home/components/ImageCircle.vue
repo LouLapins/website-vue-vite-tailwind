33dc0c05796cab38070d52e0766f7be74eabf7d0
LouLapins <loulapins@hotmail.com>
2022-07-20 21:06:08 +0200

    Move assets folder to public

--- src/pages/home/components/ImageCircle.vue
+++ src/pages/home/components/ImageCircle.vue
@@ -2,12 +2,12 @@
     <picture ref="image"
       class="translate-y-10 opacity-0 w-[45%] mb-8 mt-[5vh] lg:m-0 lg:z-10 lg:absolute lg:self-center lg:w-1/3 lg:left-[400px] xl:left-[450px] 2xl:left-[600px]">
       <source type="image/webp"
-        srcset="../../../assets/lou-river-372w.webp 372w, ../../../assets/lou-river-520w.webp 530w, ../../../assets/lou-river-743w.webp 743w"
+        srcset="/assets/lou-river-372w.webp 372w, /assets/lou-river-520w.webp 530w, /assets/lou-river-743w.webp 743w"
         class="rounded-full">
       <source type="image/jpeg"
-        srcset="../../../assets/lou-river-372w.jpg 372w, ../../../assets/lou-river-520w.jpg 520w, ../../../assets/lou-river-743w.jpg 743w"
+        srcset="/assets/lou-river-372w.jpg 372w, /assets/lou-river-520w.jpg 520w, /assets/lou-river-743w.jpg 743w"
         class="rounded-full">
-      <img src="../../../assets/lou-river-743w.jpg" alt="Lou Lapins" class="rounded-full">
+      <img src="/assets/lou-river-743w.jpg" alt="Lou Lapins" class="rounded-full">
     </picture>
 </template>
 
